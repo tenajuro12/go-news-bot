faba24e675f96886c4eee98c26a9f5c1017c0019
Tena <221939@astanait.edu.kz>
2025-05-15 23:51:08 +0500

    main

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,96 @@
 package main
 
 import (
-  "fmt"
+	"context"
+	"errors"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"go-news-bot/internal/bot"
+	"go-news-bot/internal/botkit"
+	"go-news-bot/internal/config"
+	"go-news-bot/internal/fetcher"
+	"go-news-bot/internal/notifier"
+	"go-news-bot/internal/storage"
+	"go-news-bot/internal/summary"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-
 func main() {
-  //TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-  // to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-  s := "gopher"
-  fmt.Println("Hello and welcome, %s!", s)
+	botAPI, err := tgbotapi.NewBotAPI(config.Get().TelegramBotToken)
+	if err != nil {
+		log.Printf("[ERROR] failed to create botAPI: %v", err)
+		return
+	}
 
-  for i := 1; i <= 5; i++ {
-	//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-	fmt.Println("i =", 100/i)
-  }
+	db, err := sqlx.Connect("postgres", config.Get().DatabaseDSN)
+	if err != nil {
+		log.Printf("[ERROR] failed to connect to db: %v", err)
+		return
+	}
+	defer db.Close()
+
+	var (
+		articleStorage = storage.NewArticleStorage(db)
+		sourceStorage  = storage.NewSourceStorage(db)
+		fetcher        = fetcher.New(
+			articleStorage,
+			sourceStorage,
+			config.Get().FetchInterval,
+			config.Get().FilterKeywords,
+		)
+
+		notifier = notifier.New(
+			articleStorage,
+			summary.NewOpenAISummarizer(config.Get().OpenAIKey, config.Get().OpenAIPrompt),
+			botAPI,
+			config.Get().NotificationInterval,
+			2*config.Get().FetchInterval,
+			config.Get().TelegramChannelID,
+		)
+	)
+	newsBot := botkit.New(botAPI)
+	newsBot.RegisterCmdView("start", bot.ViewCmdStart())
+	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+
+	go func(ctx context.Context) {
+		if err := fetcher.Start(ctx); err != nil {
+			if !errors.Is(err, context.Canceled) {
+				log.Printf("[ERROR] failed to run fetcher: %v", err)
+				return
+			}
+
+			log.Printf("[INFO] fetcher stopped")
+		}
+	}(ctx)
+
+	go func(ctx context.Context) {
+		if err := notifier.Start(ctx); err != nil {
+			if !errors.Is(err, context.Canceled) {
+				log.Printf("[ERROR] failed to run notifier: %v", err)
+				return
+			}
+
+			log.Printf("[INFO] notifier stopped")
+		}
+	}(ctx)
+	if err := newsBot.Start(ctx); err != nil {
+		if !errors.Is(err, context.Canceled) {
+			log.Printf("[Error] failed to start: %v", err)
+			return
+		}
+		log.Println("bot stopped")
+	}
+	<-ctx.Done()
+	log.Println("[INFO] shutting down the application")
 }
